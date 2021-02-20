d01029a2742347639e614b65e43b92b774e44013
xz8la8 <174785876@qq.com>
2021-02-20 22:10:05 +0800

    test: calc 增加分辨率

--- src/utils/__tests__/calculations.test.ts
+++ src/utils/__tests__/calculations.test.ts
@@ -5,7 +5,7 @@ describe('calculations', () => {
     expect(getProportions(0, 100)).toEqual(0)
     // expect(getProportions(50, 100)).toEqual(50)
     // expect(getProportions(11.22, 100)).toEqual(11.22)
-    // expect(getProportions(10, 11, 4)).toEqual(90.9091)
+    expect(getProportions(10, 11, 4)).toEqual(90.9091)
     // expect(getProportions(10, 11, 3)).toEqual(90.909)
   })
 })
